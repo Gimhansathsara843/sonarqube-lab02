563b2afb39f0a9ae7ee3686f3d945c76e62e05b5
Gimhansathsara843 <160720393+Gimhansathsara843@users.noreply.github.com>
2026-01-28 03:47:36 +0530

    Introduce quality issues

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -2,6 +2,7 @@ package main.java.com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserService {
@@ -10,19 +11,24 @@ public class UserService {
     private String password = "admin123";
 
     // VULNERABILITY: SQL Injection
-    public void findUser(String username) throws Exception {
+public void findUser(String username) throws SQLException {
+try(
+    Connection conn =
+        DriverManager.getConnection(
+            "jdbc:mysql://localhost/db",
+            "root",
+            password
+        );
 
-        Connection conn =
-            DriverManager.getConnection("jdbc:mysql://localhost/db",
-                    "root", password);
+    Statement st = conn.createStatement())
+{
+    String query =
+        "SELECT * FROM users WHERE name = '" + username + "'";
 
-        Statement st = conn.createStatement();
+    st.executeQuery(query);
+}
+}
 
-        String query =
-            "SELECT * FROM users WHERE name = '" + username + "'";
-
-        st.executeQuery(query);
-    }
 
     // SMELL: Unused method
     public void notUsed() {
@@ -30,16 +36,19 @@ public class UserService {
     }
 
     // EVEN WORSE: another SQL injection 
-    public void deleteUser(String username) throws Exception { 
+    public void deleteUser(String username) throws SQLException { 
+        try(
         Connection conn = 
             DriverManager.getConnection("jdbc:mysql://localhost/db", 
                 "root", password); 
 
-        Statement st = conn.createStatement(); 
+        Statement st = conn.createStatement() )
         
+        {
         String query = 
             "DELETE FROM users WHERE name = '" + username + "'"; 
     
         st.execute(query); 
+        }
 } 
 }
